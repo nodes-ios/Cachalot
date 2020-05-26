64d8a6d573551fcd19a4fa89b4562f969f03353d
Chris Combs <chco@nodes.dk>
2020-05-26 10:07:58 +0200

    Initial Commit

---
+++ Cachalot/Cachalot.h
@@ -0,0 +1,19 @@
+//
+//  Cachalot.h
+//  Cachalot
+//
+//  Created by Chris Combs on 26/05/2020.
+//  Copyright © 2020 Nodes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Cachalot.
+FOUNDATION_EXPORT double CachalotVersionNumber;
+
+//! Project version string for Cachalot.
+FOUNDATION_EXPORT const unsigned char CachalotVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cachalot/PublicHeader.h>
+
+
---
+++ CachalotTests/CachalotTests.swift
@@ -0,0 +1,34 @@
+//
+//  CachalotTests.swift
+//  CachalotTests
+//
+//  Created by Chris Combs on 26/05/2020.
+//  Copyright © 2020 Nodes. All rights reserved.
+//
+
+import XCTest
+@testable import Cachalot
+
+class CachalotTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
